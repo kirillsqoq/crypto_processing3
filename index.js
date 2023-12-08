a526c8e9460a5b5f506266c33375ee5280264b1e
Kirill <52896622+kirillsqoq@users.noreply.github.com>
2023-12-09 00:33:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const express = require("express");
+const http = require("http");
+const crypto = require("crypto");
+const secretKey =
+	"O-WI_EZeXz6IfkPbThcQ6IHEllvh-8ZvVcKbYgyaOIdcjH1Z1p9IQdaoChRJU_gX";
 
-const app = express();
-const port = 3000;
-app.use(express.json());
+const host = "127.0.0.1";
+const port = 8000;
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+const vallidateRequest = function (data) {
+	if (typeof data === "object" && data.verify_hash && secretKey) {
+		const ordered = { ...data };
+		delete ordered.verify_hash;
+		const string = JSON.stringify(ordered);
+		const hmac = crypto.createHmac("sha1", secretKey);
+		hmac.update(string);
+		const hash = hmac.digest("hex");
+		return hash === data.verify_hash;
+	}
+	return false;
+};
+
+const requestListener = function (req, res) {
+	let chunks = "";
+	req.on("data", (chunk) => (chunks += chunk));
+	req.on("end", () => {
+		let data;
+		try {
+			data = JSON.parse(chunks);
+			console.log(data);
+			res.setHeader("Content-Type", "application/json");
+		} catch (e) {
+			// console.error(e);
+			data = false;
+		}
+
+		if (data && vallidateRequest(data)) {
+			res.writeHead(200);
+			res.end("This is a correct JSON callback");
+		} else {
+			res.writeHead(422);
+			res.end("Incorrect data 1");
+		}
+	});
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+	console.log(`Server is running on http://${host}:${port}`);
 });
-app.post("/callback", async (req, res) => {
-	console.log("сработал кал бэк");
-	logObjectFields(req.body);
-
-});
-app.get("/", function (req, res) {
-	res.send("hello world");
-});
-function logObjectFields(obj) {
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      console.log(`${key}: ${obj[key]}`);
-    }
-  }
-}
-
-// Пример использования функции
-
