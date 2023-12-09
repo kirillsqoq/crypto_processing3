6396ecebbf4ab8071d25d7a992e211c2cf60ff3b
Kirill <52896622+kirillsqoq@users.noreply.github.com>
2023-12-09 11:46:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+// const express = require("express");
+
+// const app = express();
+// app.use(express.json());
+// const port = 3000;
+// app.listen(port, () => {
+// 	console.log(`Example app listening on port ${port}`);
+// });
+// app.post("/callback", async (req, res) => {
+// 	console.log("сработал кал бэк");
+// 	console.log(req);
+// 	res.setHeader("Content-Type", "application/json");
+// 	// Отправляем ответ в формате JSON
+// 	res.json({ message: "Success in JSON format" });
+// });
+// app.get("/", function (req, res) {
+// 	res.send("hello world");
+// });
 const http = require("http");
 const crypto = require("crypto");
 const secretKey =
 	"O-WI_EZeXz6IfkPbThcQ6IHEllvh-8ZvVcKbYgyaOIdcjH1Z1p9IQdaoChRJU_gX";
 
 const host = "127.0.0.1";
-const port = 8000;
+const port = 3000;
 
 const vallidateRequest = function (data) {
+	console.log(data);
 	if (typeof data === "object" && data.verify_hash && secretKey) {
+		console.log(data.status);
+
 		const ordered = { ...data };
 		delete ordered.verify_hash;
 		const string = JSON.stringify(ordered);
@@ -20,13 +41,13 @@ const vallidateRequest = function (data) {
 };
 
 const requestListener = function (req, res) {
+	
 	let chunks = "";
 	req.on("data", (chunk) => (chunks += chunk));
 	req.on("end", () => {
 		let data;
 		try {
 			data = JSON.parse(chunks);
-			console.log(data);
 			res.setHeader("Content-Type", "application/json");
 		} catch (e) {
 			// console.error(e);
