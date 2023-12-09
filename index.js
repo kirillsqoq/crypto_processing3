995e154b78eeb51de062f5fcdc5e8ed224339251
Kirill <52896622+kirillsqoq@users.noreply.github.com>
2023-12-09 13:12:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-const express = require("express");
+const express = require('express');
+const crypto = require('crypto');
+const secretKey = 'O-WI_EZeXz6IfkPbThcQ6IHEllvh-8ZvVcKbYgyaOIdcjH1Z1p9IQdaoChRJU_gX';
+
 const app = express();
-app.use(express.json());
 const port = 3000;
+
+// Используем middleware express.json() для автоматического разбора JSON данных
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  const data = req.body;
+
+  if (data && validateRequest(data)) {
+    res.status(200).json({ message: 'This is a correct JSON callback' });
+  } else {
+    res.status(422).json({ error: 'Incorrect data' });
+  }
+});
+
+function validateRequest(data) {
+  if (typeof data === 'object' && data.verify_hash && secretKey) {
+    const ordered = { ...data };
+    delete ordered.verify_hash;
+    const string = JSON.stringify(ordered);
+
+    const hmac = crypto.createHmac('sha1', secretKey);
+    hmac.update(string);
+    const hash = hmac.digest('hex');
+
+    return hash === data.verify_hash;
+  }
+  return false;
+}
+
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-});
-app.post("/callback", async (req, res) => {
-	console.log("callback requset");
-	console.log(req.body);
-	res.setHeader("Content-Type", "application/json");
-	res.json({ message: "Success in JSON format" });
-});
-app.get("/", function (req, res) {
-	res.send("hello");
+  console.log(`Server is running on http://localhost:${port}`);
 });
